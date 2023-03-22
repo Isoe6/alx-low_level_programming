26b1747b188420bff3629835a2a8e9be528890e8
Isoe6 <ochokieunice@gmail.com>
2023-03-22 04:41:31 -0700

    function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,46 +1,50 @@
-include "main.h"
-
+#include "main.h"
+#include "stdio.h"
 /**
- * print_times_table - Prints the n times table
- *
- * @n: number times table (0 < n <= 15)
- *
- * Return: no return
+ * print_times_table - check the code.
+ * @x: Description of parameter x
+ * Description
+ * Return: Always 0.
  */
-void print_times_table(int n)
+void print_times_table(int x)
 {
-	int a, b, op;
+	int i, j, n;
 
-	if (n >= 0 && n <= 15)
+	for (i = 0; i <= x && (x < 15 && x >= 0); i++)
 	{
-		for (a = 0; a <= n; a++)
+		for (j = 0; j <= x; j++)
 		{
-			_putchar(48);
-			for (b = 1; b <= n; b++)
+			n = i * j;
+			if (n > 9)
 			{
-				op = a * b;
-				_putchar(44);
-				_putchar(32);
-				if (op <= 9)
+				if (n > 99)
 				{
-					_putchar(32);
-					_putchar(32);
-					_putchar(op + 48);
+					_putchar((n / 100) + '0');
 				}
-				else if (op <= 99)
+				_putchar((n / 10) % 10 + '0');
+				_putchar(n % 10 + '0');
+			}
+			else
+			{
+				if (j != 0)
 				{
-					_putchar(32);
-					_putchar((op / 10) + 48);
-					_putchar((op % 10) + 48);
+					_putchar(' ');
 				}
-				else
+				_putchar(n + '0');
+			}
+			if (j < x)
+			{
+				_putchar(',');
+				if (n / 100 == 0  || n >= 99)
 				{
-					_putchar(((op / 100) % 10) + 48);
-					_putchar(((op / 10) % 10) + 48);
-					_putchar((op % 10) + 48);
+					_putchar(' ');
+					if ((i * (j + 1)) / 100 == 0)
+					{
+						_putchar(' ');
+					}
 				}
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
