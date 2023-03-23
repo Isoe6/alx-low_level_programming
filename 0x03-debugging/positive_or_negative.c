0eba9a74b79474f145c81b63f34a571f6dac6bd5
Isoe6 <ochokieunice@gmail.com>
2023-03-23 00:02:32 -0700

    My first debug

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,16 +1,16 @@
-#include "main.h"
-/**
- * positive_or_negative - checks for positive and negative
- * @i: to be checked
- * Return: always 0 on success
- */
+#include <stdio.h>
 
-void positive_or_negative(int i)
+/**
+ * positive_or_negative - Entry point
+ *@n: desc
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative(int n)
 {
-	if (i > 0)
-		printf("%d is positive\n", i);
-	else if (i < 0)
-		printf("%d is negative\n", i);
-	else
-		printf("%d is zero\n", i);
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
 }
